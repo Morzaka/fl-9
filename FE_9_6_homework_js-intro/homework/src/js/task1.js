024b838643ec11d03a126582196475814786a627
Morzaka <vujko007@gmail.com>
2018-08-19 23:58:18 +0300

    esLint test

--- FE_9_6_homework_js-intro/homework/src/js/task1.js
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -3,9 +3,15 @@
 const calcBtn = document.getElementById(`calculation`);
 calcBtn.addEventListener(`click`, priceCalculation);
 
+const promptAmount = `Please input your amount below`;
+const alertAmount = `Please, enter a valid amount. It should be NUMBER greater than zero!`;
+const promptDiscount = `Please enter your discount in percentage from 1 to 100`;
+const alertDiscount = `Please, use only NUMBER in range from 1 to 100!`;
+
 function getInputData(promptMessage, promptPlaceholder, alertMessage, indicator) {
   let inputNum = prompt(promptMessage, promptPlaceholder);
-  while ((isNaN(+inputNum)) || (inputNum < 0) || (parseInt(inputNum, 10) === 0) || (inputNum.length === 0) || (+inputNum > indicator)) {
+  while ((isNaN(+inputNum)) || (inputNum < 0) || (parseInt(inputNum, 10) === 0) ||
+  (inputNum.length === 0) || (+inputNum > indicator)) {
     alert(alertMessage);
     inputNum = prompt(promptMessage, promptPlaceholder);
   }
@@ -16,13 +22,13 @@ function getInputData(promptMessage, promptPlaceholder, alertMessage, indicator)
 }
 
 function priceCalculation() {
-  let amount = getInputData(`Please input your amount below`, 4950, `Please, enter a valid amount. It should be NUMBER greater than zero!`, Infinity);
-  let discount = getInputData(`Please enter your discount in percentage from 1 to 100`, 30, `Please, use only NUMBER in range from 1 to 100!`, 100);
+  let amount = getInputData(promptAmount, 4950, alertAmount, Infinity);
+  let discount = getInputData(promptDiscount, 30, alertDiscount, 100);
   let saved = (amount * discount) / 100;
   console.log(
-    `Price without discount: ${parseInt( amount * 100 ) / 100}
+    `Price without discount: ${parseInt(amount * 100) / 100}
 \nDiscount: ${discount}
-\nPrice with discount: ${parseInt( (amount - saved) * 100 ) / 100}
+\nPrice with discount: ${parseInt((amount - saved) * 100) / 100}
 \nSaved: ${parseInt(saved * 100) / 100}`
   );
 }
