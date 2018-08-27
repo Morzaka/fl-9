43a858b46d7d6cbf343ad12b831c3721d2fd3d01
Morzaka <vujko007@gmail.com>
2018-08-27 15:22:19 +0300

    finish first half of HW

--- FE_9_9_homework_data-types/homework/index.js
+++ FE_9_9_homework_data-types/homework/index.js
@@ -1,1 +1,33 @@
-// Your code goes here
+'use strict';
+
+function findType(arg) {
+  return typeof arg;
+}
+
+function forEach(array, func) {
+  for (let i = 0; i < array.length; i++) {
+    func(array[i]);
+  }
+}
+
+function map(array, func) {
+  let newArr = [];
+  forEach(array, function (el) {
+    newArr.push(func(el))
+  });
+  return newArr;
+}
+
+function filter(array, func) {
+  let newArr = [];
+  forEach(array, function (el) {
+    if (func(el)) {
+      newArr.push(func(el))
+    }
+  });
+  return newArr;
+}
+
+
+console.log(filter([2, 5, 8], function(el) { return el > 3 }));
+//console.log(findType(null)); // returns “object”
