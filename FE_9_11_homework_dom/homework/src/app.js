72c06ef6d42688fcb5998feb674ddb66af3f8029
Morzaka <vujko007@gmail.com>
2018-09-04 23:12:20 +0300

    why I do this! I have a work! I have sallary

--- FE_9_11_homework_dom/homework/src/app.js
+++ FE_9_11_homework_dom/homework/src/app.js
@@ -1,5 +1,38 @@
-var rootNode = document.getElementById("root");
+var rootNode = document.getElementById('root');
 
-// Your code goes here
+let header = document.createElement('div');
+let heading = document.createElement('h1');
+let inputField = document.createElement('input');
+let addButton = document.createElement('div');
 
-rootNode.appendChild(/* Append your list item node*/);
+header.className = 'header';
+heading.innerText = 'TODO Cat List';
+
+inputField.className = 'input-todo';
+inputField.placeholder = 'Add New Action';
+
+addButton.className = 'addItemButton';
+addButton.innerHTML = '<i class="material-icons">add_box</i>';
+
+header.appendChild(inputField);
+header.appendChild(addButton);
+header.appendChild(document.createElement('hr'));
+
+let main = document.createElement('div');
+
+main.innerHTML = createItem();
+
+function createItem() {
+  let listItem = document.createElement('li');
+  listItem.className = 'listItem';
+  const unchecked = '<i class="material-icons">check_box_outline_box</i>';
+  const bin = '<i class="material-icons">delete</i>';
+
+  return `${unchecked} Some todo text ${bin}`;
+}
+
+let container = document.createElement('div');
+container.appendChild(header);
+container.appendChild(main);
+
+rootNode.appendChild(container);
